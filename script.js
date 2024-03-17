151d60deb1a4fc947727e2a1e12358c5eef1747c
Ruchi Badkur <94377483+RuchiBadkur@users.noreply.github.com>
2024-03-17 23:01:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 // complete the given function
 
 function palindrome(str){
-
+	let newStr = "";
+	newStr = str.split("").filter(char => char !== " ").join("");
+	
+	return newStr === str.split("").reverse().join("");
+	
 }
 module.exports = palindrome
